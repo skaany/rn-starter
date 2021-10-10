433e9154501fc8dda360523f645d2a487c5920bd
Kaan Yildirim <sercankaan@hotmail.com>
2021-10-10 11:56:02 -0500

    KY | First Commit to rn-starter

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi </Text>
+      <Button onPress={() => navigation.navigate('Components')} title="Go to Components Demo" />
+      <Button onPress={() => navigation.navigate('List')} title="Go to List Demo" />
+      <Button onPress={() => navigation.navigate('Image')} title="Go to Image Screen" />
+      
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
