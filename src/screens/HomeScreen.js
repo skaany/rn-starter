121cf5f2c04a2d8e6e7fc385b78367d5f2b57a94
Kaan Yildirim <sercankaan@hotmail.com>
2021-10-12 21:56:48 -0500

    rn starter FlatList

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,10 +6,30 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi </Text>
-      <Button onPress={() => navigation.navigate('Components')} title="Go to Components Demo" />
-      <Button onPress={() => navigation.navigate('List')} title="Go to List Demo" />
-      <Button onPress={() => navigation.navigate('Image')} title="Go to Image Screen" />
-      
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to Image Screen"
+      />
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to Counter Screen"
+      />
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to Color Screen"
+      />
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to Square Screen"
+      />
     </View>
   );
 };
